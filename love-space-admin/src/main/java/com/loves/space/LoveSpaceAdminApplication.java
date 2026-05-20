777f4ec994cc8d43af13a0e1232c248513169790
lanshuangping <shuangping.lan@softgridinc.com>
2026-05-20 18:40:17 +0800

    init

---
+++ love-space-admin/src/main/java/com/loves/space/LoveSpaceAdminApplication.java
@@ -0,0 +1,13 @@
+package com.loves.space;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveSpaceAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoveSpaceAdminApplication.class, args);
+    }
+
+}
---
+++ love-space-app/src/main/java/com/space/app/LoveSpaceAppApplication.java
@@ -0,0 +1,13 @@
+package com.space.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveSpaceAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoveSpaceAppApplication.class, args);
+    }
+
+}
